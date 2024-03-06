8abbc85e641aecd8c4bf5adef208c6deaa981c23
sock_magic <startxingcheng@gmail.com>
2024-03-06 11:05:42 +0800

    chore: 注释

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -7,22 +5,22 @@ plugins {
 }
 allprojects {
     // 读取 local.properties 文件配置
-    val properties = gradleLocalProperties(rootDir)
-    val localPropertiesFile = rootProject.file("local.properties")
-    if (localPropertiesFile.exists()) {
-        localPropertiesFile.inputStream().use {
-            properties.load(it)
-        }
-    }
-
-    val buildDirPath = properties.getProperty("build.dir")
-    buildDir = if (!buildDirPath.isNullOrBlank()) {
-        // 将构建文件统一输出到指定的目录下
-        File(buildDirPath, rootProject.name + "/build/${path.replace(":", "/")}")
-    } else {
-        // 将构建文件统一输出到项目根目录下的 build 文件夹
-        File(rootDir, "build/${path.replace(":", "/")}")
-    }
+//    val properties = gradleLocalProperties(rootDir)
+//    val localPropertiesFile = rootProject.file("local.properties")
+//    if (localPropertiesFile.exists()) {
+//        localPropertiesFile.inputStream().use {
+//            properties.load(it)
+//        }
+//    }
+//
+//    val buildDirPath = properties.getProperty("build.dir")
+//    buildDir = if (!buildDirPath.isNullOrBlank()) {
+//        // 将构建文件统一输出到指定的目录下
+//        File(buildDirPath, rootProject.name + "/build/${path.replace(":", "/")}")
+//    } else {
+//        // 将构建文件统一输出到项目根目录下的 build 文件夹
+//        File(rootDir, "build/${path.replace(":", "/")}")
+//    }
 }
 
 tasks.register<Delete>("clean") {
